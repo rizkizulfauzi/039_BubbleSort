6e7f41f1b27dfe146285c7a27fb4b04e4506de52
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-03-13 11:42:07 +0700

    prosedur untuk input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,28 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input(){
+    while(true){
+        cout << "Masukan banyaknya elemen pada array :";
+        cin >> n;
+        if (n<=20)
+        break;
+        else{
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n" << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan element array" << endl;
+    cout << "======================" << endl;
+
+   for (int i - 0; <n; i++){
+    cout << "Data ke-" <<(i+i) << ";";
+   }
+
+}
+
+
+
