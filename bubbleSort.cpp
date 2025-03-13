d6e5c399a83727e60484aad112cfc08aee8ffec3
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-03-13 11:48:55 +0700

    prosedur display

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -48,5 +48,22 @@ void bubbleSortArray(){
    } while (pass <= n - 1); //step 5
 }
 
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++){
+        cout << arr[j];
+        if (j < n - 1){
+            cout << "-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = "
+}
 
 
