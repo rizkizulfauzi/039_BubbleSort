1ee0fef651fd4d056af2dba88f1aa9c91289c607
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-03-13 11:43:54 +0700

    prosedur bubblesort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -27,5 +27,26 @@ void input(){
 
 }
 
+void bubbleSortArray(){
+    int pass = 1; //step1
+
+   do 
+   {
+    for (int j = 0; j <= n -1-pass;j++)
+    {//step 2
+
+        if(arr[j] > arr[j+1]){
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+
+    pass = pass + 1; //step4
+    
+   } while (pass <= n - 1); //step 5
+}
+
 
 
